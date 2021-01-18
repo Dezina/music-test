2d658218b76d09558821cfc5c874a791806dbc33
dezinapereira1@gmail.com <dezinapereira1@gmail.com>
2021-01-18 08:01:14 +0530

    removed code which is not required

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -6,20 +6,19 @@ function playMusic() {
     // TODO Play these notes one after the other at the pitch and rhythm given in each note
 
     const beats = [];
-    var finalBeat;
-    var loop = 0;
 
     // FOR LOOP TO GET THE NOTES
     for (let i = 0; i < notes.length; i++) {
-        let a = notes[i].accidental
+
         beats.push((notes[i].pitch).concat((notes[i].octave).toString()).concat(notes[i].accidental))
 
     }
 
     // PLAY THE NOTES
-    console.log(beats.toString())
+    console.log(beats)
     for (let i = 0; i < beats.length; i++) {
-        const audio = (document.getElementById(beats[i].toString()) as HTMLAudioElement).play();
+        console.log(beats[i])
+        const audio = (document.getElementById(beats[i]) as HTMLAudioElement).play();
     }
 
 }
